c2f7bf2e906d24470f8d37d5e79d770377d0156f
ChristopherBR <68708336+ChristopherBR@users.noreply.github.com>
2020-07-23 20:48:09 +0200

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "nat" {
   instance_type               = "t2.micro"
   key_name                    = var.aws_key_name
   vpc_security_group_ids      = [aws_security_group.nat.id]
-  subnet_id                   = aws_subnet.eu-west-1a-public.id
+  subnet_id                   = aws_subnet.us-west-1a-public.id
   associate_public_ip_address = true
   source_dest_check           = false
 
